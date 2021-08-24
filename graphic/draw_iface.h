26918196aa8d083bc546b97147e6ff175ce3d5fc
CodeWasp942 <tmz50301@163.com>
2021-08-24 08:53:58 +0800

    添加项目文件。

---
+++ graphic/draw_iface.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class draw_iface {
+public:
+
+
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include "main.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello CMake." << endl;
+	system("ping -a www.shequ.codemao.cn");
+	return 0;
+}
